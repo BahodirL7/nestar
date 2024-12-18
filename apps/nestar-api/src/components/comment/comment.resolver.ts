0b3b8b06e8dc9eb485464b7ba5feb03f68482081
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-12-18 12:07:51 +0900

    feat: develop updateComment, getComments GraphQL APIs

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -1,13 +1,14 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { Comment, Comments } from '../../libs/dto/comment/comment';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
 export class CommentResolver {
@@ -24,14 +25,25 @@ export class CommentResolver {
 		return await this.commentService.createComment(memberId, input);
 	}
 
-	// @UseGuards(AuthGuard)
-	// @Mutation((returns) => Comment)
-	// public async updateComment(
-	// 	@Args('input') input: CommentUpdate,
-	// 	@AuthMember('_id') memberId: ObjectId,
-	// ): Promise<Comment> {
-	// 	console.log('Mutation: updateComment');
-	// 	input._id = shapeIntoMongoObjectId(input._id);
-	// 	return await this.commentService.updateComment(memberId, input);
-	// }
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Comment)
+	public async updateComment(
+		@Args('input') input: CommentUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('Mutation: updateComment');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.commentService.updateComment(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Comments)
+	public async getComments(
+		@Args('input') input: CommentsInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comments> {
+		console.log('Query: getComments');
+		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+		return await this.commentService.getComments(memberId, input);
+	}
 }
--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -1,14 +1,16 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { Comment } from '../../libs/dto/comment/comment';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../member/member.service';
 import { PropertyService } from '../property/property.service';
 import { BoardArticleService } from '../board-article/board-article.service';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
-import { Message } from '../../libs/enums/common.enum';
-import { CommentGroup } from '../../libs/enums/comment.enum';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { T } from '../../libs/types/common';
+import { lookupMember } from '../../libs/config';
 
 @Injectable()
 export class CommentService {
@@ -57,5 +59,44 @@ export class CommentService {
 		return result;
 	}
 
-	// public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<any> {}
+	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
+		const { _id } = input;
+		const result = await this.commentModel.findOneAndUpdate(
+			{
+				_id: _id,
+				memberId: memberId,
+				commentStatus: CommentStatus.ACTIVE,
+			},
+			input,
+			{ new: true },
+		);
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+		return result;
+	}
+
+	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+		const { commentRefId } = input.search;
+		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+		const result: Comments[] = await this.commentModel.aggregate([
+			{ $match: match },
+			{ $sort: sort },
+			{
+				$facet: {
+					list: [
+						{ $skip: (input.page - 1) * input.limit },
+						{ $limit: input.limit },
+						// meLiked
+						lookupMember,
+						{ $unwind: '$memberData' },
+					],
+					metaCounter: [{ $count: 'total' }],
+				},
+			},
+		]);
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
+	}
 }
