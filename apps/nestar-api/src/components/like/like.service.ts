1d7e7f11b9ecdc6cfd4d4faae0d4e434e7b22d58
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-12-20 00:11:03 +0900

    feat: develop likeTargetMember GraphQL api

--- apps/nestar-api/src/components/like/like.service.ts
+++ apps/nestar-api/src/components/like/like.service.ts
@@ -1,17 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Like } from '../../libs/dto/like/like';
 import { MemberService } from '../member/member.service';
 import { PropertyService } from '../property/property.service';
 import { BoardArticleService } from '../board-article/board-article.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { T } from '../../libs/types/common';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class LikeService {
-	constructor(
-		@InjectModel('Like') private readonly likeModel: Model<Like>,
-		private readonly memberService: MemberService,
-		private readonly propertyService: PropertyService,
-		private readonly boardArticleService: BoardArticleService,
-	) {}
+	constructor(@InjectModel('Like') private readonly likeModel: Model<Like>) {}
+
+	public async toggleLike(input: LikeInput): Promise<number> {
+		const search: T = { memberId: input.memberId, likeRefId: input.likeRefId };
+		const existing = await this.likeModel.findOne(search).exec();
+		let modifier: number;
+
+		if (existing) {
+			await this.likeModel.findOneAndDelete(search).exec();
+			modifier = -1; // Unlike
+		} else {
+			try {
+				await this.likeModel.findOneAndUpdate(search, input, { upsert: true, setDefaultsOnInsert: true }).exec();
+				modifier = 1; // Like
+			} catch (err) {
+				console.error('Error, Service.model', err.message);
+				throw new BadRequestException('Create is failed!');
+			}
+		}
+
+		console.log(`- Like modifier ${modifier} -`);
+		return modifier;
+	}
 }
